90140464d3e308b95bf91ca60c2a758a15bc5171
felipecr <felipecrbr@gmail.com>
2022-11-15 11:14:25 -0300

    Atualizações na console app.

--- health-calc-console-dotnet/Program.cs
+++ health-calc-console-dotnet/Program.cs
@@ -1,14 +1,73 @@
 ﻿using health_calc_pack_dotnet;
+using health_calc_pack_dotnet.Enums;
+using health_calc_pack_dotnet.Models;
 
-Console.WriteLine("Digite sua altura e peso para calcular seu IMC.");
+try
+{
+    Console.WriteLine("Digite sua altura e peso para calcular seu IMC.");
 
-Console.Write("Altura: ");
-double height = double.Parse(Console.ReadLine());
+    Console.Write("Altura: ");
 
-Console.Write("Peso: ");
-double weight = double.Parse(Console.ReadLine());
+    string? input1 = Console.ReadLine();
 
-BMI bmi = new BMI();
+    double height = 0;
 
-double result = bmi.Calculate(height, weight);
-Console.WriteLine(result);
+    if (input1 != null)
+    {
+        height = double.Parse(input1);
+    }
+
+    Console.Write("Peso: ");
+
+    string? input2 = Console.ReadLine();
+
+    double weight = 0;
+
+    if (input2 != null)
+    {
+        weight = double.Parse(input2);
+    }
+
+    BMI bmi = new BMI();
+
+    if (height > 0 && weight > 0)
+    {
+        double result = bmi.Calculate(height, weight);        
+        Console.WriteLine("IMC calculado: " + result);
+
+        string classification = bmi.GetClassification(result);
+        Console.WriteLine("Classificação: " + classification);
+
+        Console.WriteLine("Digite seu sexo para calcular os macronutrientes.");
+
+        Console.Write("Sexo (F ou M): ");
+
+        string? input3 = Console.ReadLine();
+
+        if (input3 != null && (input3.ToUpper() == "F" || input3.ToUpper() == "M"))
+        {
+            Gender gender = input3.ToUpper() == "F" ? Gender.Female : Gender.Male;
+
+            Macronutrient macronutrient = new Macronutrient();
+            MacronutrientModel model = macronutrient.Calculate(gender, height, weight, PhysicalActivityLevel.ModeratelyActive, PhysicalGoal.Maintenance);
+
+            Console.WriteLine("Macronutrientes calculados para manutenção de peso de pessoa moderadamente ativa:");
+            Console.WriteLine("-Carboidratos: " + model.Carbohydrates);
+            Console.WriteLine("-Proteínas: " + model.Proteins);
+            Console.WriteLine("-Gorduras: " + model.Fats);
+        }
+        else
+        {
+            Console.WriteLine("Sexo incorreto.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Altura e/ou peso incorretos.");
+    }
+}
+catch (Exception)
+{
+    Console.WriteLine("Algo deu errado. Por favor, revise os dados e tente novamente.");
+    throw;
+}
